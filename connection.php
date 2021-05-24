a21bba3d573ef68a3a873684afb8f4301faab12d
Mahmoud <mahmoud.diab@influitive.com>
2021-05-24 21:56:19 +0300

    working on the php file to connect to mysql

---
+++ connection.php
@@ -0,0 +1,41 @@
+<?php 
+    // Connection details
+	$servername = "localhost:3306";
+    $username = "root";
+    $password = "";
+    $dbname = "diab";
+    // Create a connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if($conn->connect_error){
+        die("Failed to connect: " .$conn->connect_error);
+    }
+    echo "Connected successfully!"."<br />";
+
+
+    // MYSQL Commands
+    $sql = "SELECT * FROM manal";
+    $result = $conn->query($sql);
+    if($result == TRUE){
+        // Show table content
+        if($result-> num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                echo $row["ItemId"]."<br />";
+                echo $row["ItemName"]."<br />";
+                echo $row["Price"]."<br />";
+                echo $row["Quantity"]."<br />";
+                echo $row["Price"]*$row["Quantity"]."<br />";
+                echo "============"."<br />";
+
+            }
+            $result->free();
+        }
+        echo "0 results";
+    }
+    else {
+        echo "Failed: ".$conn->error;
+    }
+    // Close connection
+    // mysqli_close($conn);
+
+ ?>
